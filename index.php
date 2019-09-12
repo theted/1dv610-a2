f4be13f9c8902cbd88f25f3b179e9f6237fd792d
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-09-12 23:30:04 +0200

    Update dependencies, rename variables to increase understandability

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <?php
 
 //INCLUDE THE FILES NEEDED...
-require_once('view/LoginView.php');
-require_once('view/DateTimeView.php');
-require_once('view/LayoutView.php');
+require_once 'controller/LoginController.php';
+require_once 'controller/RegisterController.php';
+require_once 'view/LoginView.php';
+require_once 'view/RegisterView.php';
+require_once 'view/DateTimeView.php';
+require_once 'view/LayoutView.php';
 
 //MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
 //CREATE OBJECTS OF THE VIEWS
-$v = new LoginView();
-$dtv = new DateTimeView();
-$lv = new LayoutView();
+$loginController = new LoginController();
+$registerController = new RegisterController();
+$loginPage = new LoginView($loginController);
+$registerPage = new RegisterView($registerController);
+$dateTime = new DateTimeView();
+$layout = new LayoutView();
 
+// set which view to show (register or login), depending on query parameters...
+// TODO: implement a router instead of this hacky stuff
+$mainPage = (isset($_GET['register'])) ? $registerPage : $loginPage;
 
-$lv->render(false, $v, $dtv);
-
+// render page using layout view
+$layout->render(false, $mainPage, $dateTime);
