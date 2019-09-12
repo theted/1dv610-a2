0ab9d8cb0f08dfea5ce3c31d4aa691cd8a7eece3
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-09-12 23:35:09 +0200

    Add logged-in section, update indentation (using phpfmt)

--- view/LoginView.php
+++ view/LoginView.php
@@ -1,58 +1,85 @@
 <?php
 
-class LoginView {
-	private static $login = 'LoginView::Login';
-	private static $logout = 'LoginView::Logout';
-	private static $name = 'LoginView::UserName';
-	private static $password = 'LoginView::Password';
-	private static $cookieName = 'LoginView::CookieName';
-	private static $cookiePassword = 'LoginView::CookiePassword';
-	private static $keep = 'LoginView::KeepMeLoggedIn';
-	private static $messageId = 'LoginView::Message';
+class LoginView
+{
+    private static $login = 'LoginView::Login';
+    private static $logout = 'LoginView::Logout';
+    private static $name = 'LoginView::UserName';
+    private static $password = 'LoginView::Password';
+    private static $cookieName = 'LoginView::CookieName';
+    private static $cookiePassword = 'LoginView::CookiePassword';
+    private static $keep = 'LoginView::KeepMeLoggedIn';
+    private static $messageId = 'LoginView::Message';
+    private $error;
 
-	
+    public function __construct($controller)
+    {
+        // TODO: get input data..
+        $this->error = $controller->getError();
+    }
 
-	/**
-	 * Create HTTP response
-	 *
-	 * Should be called after a login attempt has been determined
-	 *
-	 * @return  void BUT writes to standard output and cookies!
-	 */
-	public function response() {
-		$message = '';
-		
-		$response = $this->generateLoginFormHTML($message);
-		//$response .= $this->generateLogoutButtonHTML($message);
-		return $response;
-	}
+    /**
+     * Create HTTP response
+     *
+     * Should be called after a login attempt has been determined
+     *
+     * @return  void BUT writes to standard output and cookies!
+     */
+    public function response()
+    {
+        $message = $this->error;
+        $response = "";
 
-	/**
-	* Generate HTML code on the output buffer for the logout button
-	* @param $message, String output message
-	* @return  void, BUT writes to standard output!
-	*/
-	private function generateLogoutButtonHTML($message) {
-		return '
+        if ($this->error) {
+            $response .= "<p class=\"error\">$this->error</p>";
+        }
+
+        // handle user logged-in state
+        // TODO: handle state in separate model
+        if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']) {
+            $response .= "<h2>Logged in</h2><br> as " . $_SESSION['username'] . '<br />';
+            $response .= $this->generateLogoutButtonHTML($message);
+        } else {
+
+            // register button
+            $response .= "<a href=\"?register\">Register</a>";
+
+            // login form
+            $response .= $this->generateLoginFormHTML($message);
+
+        }
+
+        return $response;
+    }
+
+    /**
+     * Generate HTML code on the output buffer for the logout button
+     * @param $message, String output message
+     * @return  void, BUT writes to standard output!
+     */
+    private function generateLogoutButtonHTML($message)
+    {
+        return '
 			<form  method="post" >
-				<p id="' . self::$messageId . '">' . $message .'</p>
+				<p id="' . self::$messageId . '">' . $message . '</p>
 				<input type="submit" name="' . self::$logout . '" value="logout"/>
 			</form>
 		';
-	}
-	
-	/**
-	* Generate HTML code on the output buffer for the logout button
-	* @param $message, String output message
-	* @return  void, BUT writes to standard output!
-	*/
-	private function generateLoginFormHTML($message) {
-		return '
-			<form method="post" > 
+    }
+
+    /**
+     * Generate HTML code on the output buffer for the logout button
+     * @param $message, String output message
+     * @return  void, BUT writes to standard output!
+     */
+    private function generateLoginFormHTML($message)
+    {
+        return '
+			<form method="post" >
 				<fieldset>
 					<legend>Login - enter Username and password</legend>
 					<p id="' . self::$messageId . '">' . $message . '</p>
-					
+
 					<label for="' . self::$name . '">Username :</label>
 					<input type="text" id="' . self::$name . '" name="' . self::$name . '" value="" />
 
@@ -61,16 +88,17 @@ class LoginView {
 
 					<label for="' . self::$keep . '">Keep me logged in  :</label>
 					<input type="checkbox" id="' . self::$keep . '" name="' . self::$keep . '" />
-					
+
 					<input type="submit" name="' . self::$login . '" value="login" />
 				</fieldset>
 			</form>
 		';
-	}
-	
-	//CREATE GET-FUNCTIONS TO FETCH REQUEST VARIABLES
-	private function getRequestUserName() {
-		//RETURN REQUEST VARIABLE: USERNAME
-	}
-	
+    }
+
+    //CREATE GET-FUNCTIONS TO FETCH REQUEST VARIABLES
+    private function getRequestUserName()
+    {
+        //RETURN REQUEST VARIABLE: USERNAME
+    }
+
 }
