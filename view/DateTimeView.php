258d1a5d941043a8e16f8cefffc2674da38d6d05
Fredrik Sundberg <fredrik@sundbergmedia.com>
2019-09-12 11:38:38 +0200

    Get current date & time

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -1,12 +1,17 @@
 <?php
 
+/*
+ * Get current date & time
+ */
+
 class DateTimeView {
 
-
 	public function show() {
-
-		$timeString = 'TODO, Write servertime here...';
-
-		return '<p>' . $timeString . '</p>';
+		$time_ = date ('H:i:s');
+		$day_ = date('l');
+		$date_ = date('jS') . " of " . date('F') . " " . date('Y');
+		$timeString = "$day_, the $date_, The time is  $time_";
+		return "<p>$timeString</p>";
 	}
+
 }
